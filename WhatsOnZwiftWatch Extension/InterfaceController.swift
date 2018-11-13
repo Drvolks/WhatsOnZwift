667a283e8aba08e9a83a26e3e0e33957936d85bd
Jean-Francois Dufour <jfdufour@me.com>
2018-11-12 19:02:00 -0500

    Corriger appel http sur watch

--- WhatsOnZwiftWatch Extension/InterfaceController.swift
+++ WhatsOnZwiftWatch Extension/InterfaceController.swift
@@ -16,25 +16,47 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var futureTable: WKInterfaceTable!
     @IBOutlet weak var nextLabel: WKInterfaceLabel!
     
+    var task: URLSessionDataTask?
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        nextLabel.setHidden(true)
+        map.setHidden(true)
+        
         nextLabel.setHeight(CGFloat(50))
         
-        let parser = ZwiftXmlParser()
-        let appointments = parser.parse()
-        let currentAppointment = AppointmentUtils.getCurrent(appointments: appointments)
-        let futureAppointments = AppointmentUtils.getFutures(appointments: appointments)
+        let url = NSURL(string:ZwiftXmlParser.urlString)!
+        let conf = URLSessionConfiguration.default
+        let session = URLSession(configuration: conf)
         
-        map.setText(AppointmentUtils.getName(appointment: currentAppointment))
-        mapImage.setImage(UIImage(named: currentAppointment.map.rawValue.lowercased()))
-        
-        futureTable.setNumberOfRows(futureAppointments.count, withRowType: "appointmentRow")
-        
-        for index in 0..<futureTable.numberOfRows {
-            guard let controller = futureTable.rowController(at: index) as? AppointmentRowController else { continue }
-            controller.fromAppointment(appointment: futureAppointments[index])
+        task = session.dataTask(with: url as URL) { (data, res, error) -> Void in
+            if error != nil {
+                print("dataTaskWithURL fail")
+                return
+            }
+            if let data = data {
+                let parser = ZwiftXmlParser(xmlData: data)
+                let appointments = parser.parse()
+                let currentAppointment = AppointmentUtils.getCurrent(appointments: appointments)
+                let futureAppointments = AppointmentUtils.getFutures(appointments: appointments)
+                
+                self.map.setText(AppointmentUtils.getName(appointment: currentAppointment))
+                self.mapImage.setImage(UIImage(named: currentAppointment.map.rawValue.lowercased()))
+                
+                self.map.setHidden(false)
+                self.nextLabel.setHidden(false)
+                
+                self.futureTable.setNumberOfRows(futureAppointments.count, withRowType: "appointmentRow")
+                
+                for index in 0..<self.futureTable.numberOfRows {
+                    guard let controller = self.futureTable.rowController(at: index) as? AppointmentRowController else { continue }
+                    controller.fromAppointment(appointment: futureAppointments[index])
+                }
+            }
         }
+    
+        task!.resume()
     }
     
     override func willActivate() {
